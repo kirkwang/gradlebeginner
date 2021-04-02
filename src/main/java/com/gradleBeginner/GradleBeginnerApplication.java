6f0855d58322dea2b6ff83cf86e310cb0b64b6c2
kirkwang <kirkwangke@gmail.com>
2021-04-02 15:47:21 -0700

    checkInQueue project settings

---
+++ src/main/java/com/gradleBeginner/GradleBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.gradleBeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleBeginnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradleBeginner/GradleBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradleBeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
